6bd03655e28c628dc531bb531bef8d746734f849
Tran Minh An <minhan@Trans-MacBook-Pro.local>
2022-05-21 02:49:41 +0700

    demo-database

--- server/src/models/store.model.js
+++ server/src/models/store.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-const StoreScheme = new Schema(
+const StoreSchema = new Schema(
     {
         name: {
             type: String,
@@ -25,6 +25,129 @@ const StoreScheme = new Schema(
         timestamps: true,
     }
 );
+
+// 
+const StoreInfoSchema = new Schema(
+    {
+        id_Store: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+        name_Store: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+        address: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+        type: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+        phoneNumber: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+        name_Staff: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+        logo: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+        openingDay: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+        createDay: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const AbcStaffSchema = new Schema(
+    {
+        id_Abc: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+        name_Abc: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+        sex_Abc: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const StoreStaffSchema = new Schema(
+    {
+        id_Staff: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+        name_Staff: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+        name_Store: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+        sex_Staff: {
+            type: String,
+            trim: true,
+            minLength: 2,
+            maxLength: 100,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+//
 
 const StoreModel = mongoose.model('Store', StoreScheme);
 
